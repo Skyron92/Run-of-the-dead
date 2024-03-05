2d9c2fa4807dac25199b7721c66ee560859278c8
Skyron <courtier.leo@gmail.com>
2024-03-05 09:19:56 +0100

    Upgrade IMinigame interface & event

--- Assets/Leo/Scripts/MiniGame/Global/IMiniGame.cs
+++ Assets/Leo/Scripts/MiniGame/Global/IMiniGame.cs
@@ -1,7 +1,9 @@
-﻿/// <summary>
+﻿using System.Reflection;
+
+/// <summary>
 /// All mini games must implement this interface
 /// </summary>
 public interface IMiniGame {
-    
     public delegate void MiniGameWonEvent(object sender, MiniGameEventArgs e);
+    public event MiniGameWonEvent MiniGameWon;
 }
---
+++ Assets/Leo/Scripts/MiniGame/Global/MiniGameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class MiniGameManager : MonoBehaviour
+{
+    private IMiniGame _miniGameReference;
+
+    private void BindEvent() {
+        if(_miniGameReference == null) return;
+        _miniGameReference.MiniGameWon += MyMethod();
+
+    }
+
+    private IMiniGame.MiniGameWonEvent MyMethod()
+    {
+        throw new NotImplementedException();
+    }
+}
