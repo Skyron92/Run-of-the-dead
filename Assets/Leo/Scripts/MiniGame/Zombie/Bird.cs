1b1d74f4942315ad8d678bbf01975be75305d8e1
Skyron <courtier.leo@gmail.com>
2024-03-05 11:17:39 +0100

    Add Bird movement and polish bird

--- Assets/Leo/Scripts/MiniGame/Zombie/Bird.cs
+++ Assets/Leo/Scripts/MiniGame/Zombie/Bird.cs
@@ -1,12 +1,17 @@
 ﻿using DG.Tweening;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 namespace MiniGame.Zombie {
     /// <summary>
     /// Manages the bird's behaviour
     /// </summary>
     public class Bird : MonoBehaviour, IMiniGame {
+        
+        [Header("Inputs settings")]
         // Position input
         [SerializeField] private InputActionReference positionInputActionReference;
         private Vector2 InputPosition => positionInputActionReference.action.ReadValue<Vector2>();
@@ -15,13 +20,22 @@ namespace MiniGame.Zombie {
         [SerializeField] private InputActionReference touchInputActionReference;
         private InputAction TouchInputAction => touchInputActionReference.action;
 
+        [Header("Die settings")]
+        [SerializeField, Range(0.1f, 1f)] private float fallDuration;
         private RectTransform _selfRectTransform;
         [SerializeField] private RectTransform groundMinTransform;
         [SerializeField] private RectTransform zombieTransform;
 
         private bool _isDead;
 
-        [SerializeField, Range(0.1f, 1f)] private float fallDuration;
+        private int _life;
+        private int Life {
+            get { return _life; }
+            set => _life = _life < 0 ? 0 : value;
+        }
+
+        private Image _image;
+        [SerializeField, Range(0.1f, 1f)] private float fadeDuration;
 
         // Event when the player win
         public event IMiniGame.MiniGameWonEvent MiniGameWon;
@@ -30,6 +44,8 @@ namespace MiniGame.Zombie {
             CheckIfVariablesIsAssigned();
             _selfRectTransform = GetComponent<RectTransform>();
             SetupInputs();
+            Life = Random.Range(1, 3);
+            _image = GetComponent<Image>();
         }
 
         private void CheckIfVariablesIsAssigned() {
@@ -41,7 +57,7 @@ namespace MiniGame.Zombie {
             positionInputActionReference.action.Enable();
             TouchInputAction.Enable();
             TouchInputAction.started += context => {
-                if (!_isDead && PositionIsValid()) Die();
+                if (!_isDead && PositionIsValid()) Hit();
             };
             TouchInputAction.canceled += context => {
                 if(_isDead) Fall();
@@ -72,11 +88,19 @@ namespace MiniGame.Zombie {
             return _selfRectTransform != null && RectTransformUtility.RectangleContainsScreenPoint(_selfRectTransform, InputPosition);
         }
 
+        void Hit() {
+            _image.DOColor(Color.red, fadeDuration).onComplete += () => _image.DOColor(Color.white, fadeDuration);
+            Life--;
+            if(Life == 0) Die();
+        }
+
         /// <summary>
         /// Kill the bird
         /// </summary>
         private void Die() {
             _isDead = true;
+            BirdMovement birdMovement = gameObject.GetComponent<BirdMovement>();
+            birdMovement.StopMoving();
             Fall();
         }
 
---
+++ Assets/Leo/Scripts/MiniGame/Zombie/BirdMovement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MiniGame.Zombie
+{
+    public class BirdMovement : MonoBehaviour {
+
+        [Header("Move settings")]
+        [SerializeField] private RectTransform bottomLeftTransform;
+        [SerializeField] private RectTransform topRightTransform;
+        private float Left => bottomLeftTransform.position.x;
+        private float Right => topRightTransform.position.x;
+        private float Up => topRightTransform.position.y;
+        private float Down => bottomLeftTransform.position.y;
+        [SerializeField, Range(0.5f, 5f)] private float moveSpeed;
+
+        private Vector2 _destination;
+
+        private RectTransform _selfRectTransform;
+
+        private TweenerCore<Vector3, Vector3, VectorOptions> tweenerCore;
+        private void Awake() {
+            _selfRectTransform = GetComponent<RectTransform>();
+        }
+
+        private void OnEnable() {
+            Move();
+        }
+
+        private void Move() {
+            SetUpDestination();
+            tweenerCore = _selfRectTransform.DOMove(_destination, moveSpeed);
+            tweenerCore.onComplete += () => Move();
+        }
+
+        public void StopMoving() {
+            tweenerCore.Kill();
+            Destroy(this);
+        }
+        
+        private void SetUpDestination() {
+            _destination = new Vector2(Random.Range(Left, Right), Random.Range(Down, Up));
+        }
+    }
+}
