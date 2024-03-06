0f4ef81e81bafe032de78a626423f8355b32c780
Tom MASTANTUONO <compte.spidlle@gmail.com>
2024-03-06 17:52:22 +0100

    RouteFBX + material

--- Assets/Main/Scripts/Roads/Road.cs
+++ Assets/Main/Scripts/Roads/Road.cs
@@ -8,21 +8,22 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class Road : MonoBehaviour
 {
+    [SerializeField] private Transform EndRoadRef;
     private void Update() {
         Move();
     }
-
+    
     private void Move()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y,
-            transform.position.z - RoadsManager.Speed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - RoadsManager.Speed * Time.deltaTime);
     }
 
     /// <summary>
     /// Calculates the next's road position 
     /// </summary>
     /// <returns>The position of the next road</returns>
-    public Vector3 GetNextPosition() {
-        return new Vector3(transform.position.x, transform.position.y, RoadsManager.currentRoad.transform.position.z + (RoadsManager.currentRoad.transform.localScale.z / 2f + transform.localScale.z / 2f));
+    public Vector3 GetNextPosition() 
+    {
+        return new Vector3(transform.position.x, transform.position.y, Vector3.Distance(RoadsManager.currentRoad.EndRoadRef.position, RoadsManager.currentRoad.transform.position));
     }
 }
