af8f77155d4f6323d94901917e1492cd49c64e0a
Tom MASTANTUONO <compte.spidlle@gmail.com>
2024-03-11 16:19:54 +0100

    remove spawnmanager / start obstacle

---
+++ Assets/Main/Scripts/Roads/Obstacle.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
