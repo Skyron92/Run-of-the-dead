27bafd6343ff5f73a5f5ef425314641372741cc3
Skyron <courtier.leo@gmail.com>
2024-03-11 13:21:19 +0100

    Add jump

--- Assets/Main/Scripts/Character/Character.cs
+++ Assets/Main/Scripts/Character/Character.cs
@@ -1,5 +1,8 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Manages the player character
@@ -11,6 +14,14 @@ public class Character : MonoBehaviour
 
     // Speed of the right-left slide character
     [SerializeField, Range(0, 1)] private float offsetSpeed;
+
+    [SerializeField] private InputActionReference slideInputActionReference;
+    [SerializeField] private InputActionReference tapInputActionReference;
+    [SerializeField] private InputActionReference positionInputActionReference;
+    private InputAction TapInputAction => tapInputActionReference.action;
+    private InputAction SlideInputAction => slideInputActionReference.action;
+    private Vector2 SlideInputValue => SlideInputAction.ReadValue<Vector2>();
+    private Vector2 PositionInputValue => positionInputActionReference.action.ReadValue<Vector2>();
     
     // Current position index
     private int _actualSpot = 1;
@@ -41,11 +52,33 @@ public class Character : MonoBehaviour
         // Set the initial value at 1, the middle spot point index
         ActualSpot = 1;
         Current = this;
+        SlideInputAction.Enable();
+        TapInputAction.Enable();
+        positionInputActionReference.action.Enable();
+
+        TapInputAction.started += context => {
+            SetDestination(GetTargetIndex());
+        };
+        SlideInputAction.started += context => {
+            Jump();
+        };
+    }
+
+    private void Start()
+    {
+        transform.DOJump(new Vector3(0, 2, 2), 2, 1, 1f);
     }
 
     // Update is called once per frame
     void Update() {
         if(_isMoving) Move();
+    }
+
+    private int GetTargetIndex() {
+        bool left = PositionInputValue.x < Screen.width / 2;
+        bool right = PositionInputValue.x > Screen.width / 2;
+        if (_actualSpot == 0 && left || _actualSpot == 2 && right) return _actualSpot;
+        return left ? -1 : 1;
     }
 
     /// <summary>
@@ -68,6 +101,11 @@ public class Character : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, spots[_actualSpot].position, offsetSpeed);
         // Stop the movement if the character has reached the destination
         if (Vector3.Distance(transform.position, spots[_actualSpot].position) <= .1f) _isMoving = false;
+    }
+
+    private void Jump() {
+        if(SlideInputValue.y <= 0.5f) return;
+       // transform.DOJump(new Vector3(0, 2, 2), 2, 1, 1f);
     }
 
     private void OnTriggerEnter(Collider other) {
