2d906172b9ee9cfa5c389e07dcd612c4d21cc252
Spidhub <compte.spidlle@gmail.com>
2024-03-14 10:34:10 +0100

    AvantPackagemanager

--- Assets/Leo/Scripts/Runner/ZombieProgression.cs
+++ Assets/Leo/Scripts/Runner/ZombieProgression.cs
@@ -6,32 +6,26 @@ using DG.Tweening.Plugins.Options;
 using UnityEngine;
 using UnityEngine.Pool;
 using UnityEngine.UI;
-
 public class ZombieProgression : MonoBehaviour
 {
     private Slider _slider;
-
     [SerializeField] private Image fillPicture;
     [SerializeField, Range(0, 1)] private float fadeDuration;
-
-    private const float StartSpeed = 5f;
     public static float CurrentSpeed; 
-
     private TweenerCore<Color, Color, ColorOptions> _tweener;
-    
     public delegate void EventHandler(object sender, EventArgs e);
     public event EventHandler GameOver;
-
     [SerializeField] private AnimationCurve progressSpeedCurve;
+    // public TensorFloat Sigmoid(TensorFloat X);
     private void Awake() {
         _slider = GetComponent<Slider>();
-        CurrentSpeed = StartSpeed;
     }
 
     private void Update() {
         StartCoroutine(Progress());
     }
-
+    
+    
     private IEnumerator Progress() {
         _slider.value = CurrentSpeed - RoadsManager.CurrentSpeed;
         if(IsCloseOfMax() && _tweener is not { active: true }) ColorAnimation();
@@ -39,7 +33,7 @@ public class ZombieProgression : MonoBehaviour
         if (IsDead()) GameOver?.Invoke(this, EventArgs.Empty);
         yield return new WaitForSeconds(0.1f);
     }
-
+    
     private bool IsCloseOfMax() {
         return _slider.maxValue - _slider.value < _slider.maxValue / 10f;
     }
