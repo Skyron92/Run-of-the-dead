87c943c5bf2d7d12b35b635eb0e7eb259f394568
Spidhub <compte.spidlle@gmail.com>
2024-03-18 15:40:40 +0100

    Accel

--- Assets/Main/Scripts/Roads/Obstacle.cs
+++ Assets/Main/Scripts/Roads/Obstacle.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    bool isInvuln = false;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !isInvuln)
-        {
-            isInvuln = true;
-            var targetspeed = RoadsManager.BaseSpeed;
-            RoadsManager.CurrentSpeed = 1;
-            Handheld.Vibrate();
-            // Utilisez la méthode DOVirtual.Float de Dotween pour interpoler la valeur de 1 à 15 sur une durée de 15 secondes
-            DOTween.To(() => RoadsManager.CurrentSpeed, x => RoadsManager.CurrentSpeed = x, RoadsManager.BaseSpeed, 1.5f)
-                .OnComplete(() => isInvuln = false);
-        }
+        //     if (other.CompareTag("Player") && !isInvuln)
+        //     {
+        //         isInvuln = true;
+        //         var targetspeed = RoadsManager.BaseSpeed;
+        //         RoadsManager.CurrentSpeed = 1;
+        //         Handheld.Vibrate();
+        //         // Utilisez la méthode DOVirtual.Float de Dotween pour interpoler la valeur de 1 à 15 sur une durée de 15 secondes
+        //         DOTween.To(() => RoadsManager.CurrentSpeed, x => RoadsManager.CurrentSpeed = x, RoadsManager.BaseSpeed, 1.5f)
+        //             .OnComplete(() => isInvuln = false);
+        //     }
+        // }
     }
 }
