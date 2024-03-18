025ea99b5376fdab25397098e2b12f79e8a08547
Skyron <courtier.leo@gmail.com>
2024-03-18 18:48:12 +0100

    Intergation UI + start SFX picture updater

---
+++ Assets/Leo/Scripts/Sound SFX Updater.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSFXUpdater : MonoBehaviour
+{
+    [SerializeField] private Sprite full, mid, little;
+    [SerializeField] private GameObject shutdown;
+    [SerializeField] private Image sfxPicture;
+}
