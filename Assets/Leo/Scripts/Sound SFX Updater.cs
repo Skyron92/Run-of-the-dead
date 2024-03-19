61d1f039b68a089ab558124c5a671fda2c0996c1
Skyron <courtier.leo@gmail.com>
2024-03-19 14:55:51 +0100

    SFX logo updater

--- Assets/Leo/Scripts/Sound SFX Updater.cs
+++ Assets/Leo/Scripts/Sound SFX Updater.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,4 +7,36 @@ public class SoundSFXUpdater : MonoBehaviour
     [SerializeField] private Sprite full, mid, little;
     [SerializeField] private GameObject shutdown;
     [SerializeField] private Image sfxPicture;
+
+    private void Awake()
+    {
+        UpdateSFXSprite(-20);
+    }
+
+    public void UpdateSFXSprite(float value) {
+        switch (value) {
+            case >= -20f :
+                sfxPicture.sprite = full;
+                sfxPicture.enabled = true;
+                shutdown.SetActive(false);
+                break;
+            case < -20f and >= -40f :
+                sfxPicture.sprite = mid;
+                sfxPicture.enabled = true;
+                shutdown.SetActive(false);
+                break;
+            case < -40f and >= -60f :
+                sfxPicture.sprite = little;
+                sfxPicture.enabled = true;
+                shutdown.SetActive(false);
+                break;
+            case < -60f and > -80f :
+                sfxPicture.enabled = false;
+                shutdown.SetActive(false);
+                break;
+            case <= -80f :
+                shutdown.SetActive(true);
+                break;
+        }
+    }
 }
