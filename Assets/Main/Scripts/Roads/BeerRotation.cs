8da8ef8f70d94a79be3440c3d9917f255137edcd
Yunooii <kroma18102002@gmail.com>
2024-03-20 17:35:54 +0100

    beaucoup

---
+++ Assets/Main/Scripts/Roads/BeerRotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeerRoation : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+
+    void Update()
+    {
+        transform.Rotate(0,1,0);
+    }
+}
