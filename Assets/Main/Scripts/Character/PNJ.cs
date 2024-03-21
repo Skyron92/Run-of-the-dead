5f53666f813077cb79e0ffa1f0278aadff45a8b8
Tom MASTANTUONO <compte.spidlle@gmail.com>
2024-03-21 16:30:09 +0100

    lepointdelexclamationilbouge

--- Assets/Main/Scripts/Character/PNJ.cs
+++ Assets/Main/Scripts/Character/PNJ.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -13,12 +14,25 @@ public class PNJ : MonoBehaviour
     [SerializeField] private GameObject dialogBox;
     [SerializeField] private List<Sprite> headSprite;
     [SerializeField] private List<GameObject> prefabList;
+    [SerializeField] private GameObject bubble;
     private int _index;
+    
     private void Awake() {
         _index = Random.Range(0, prefabList.Count);
         prefabList[_index].SetActive(true);
     }
 
+    private void Start()
+    {
+        movebubble();
+    }
+
+    private void movebubble()
+    {
+        float patate = bubble.transform.position.y;
+        bubble.transform.DOMoveY(bubble.transform.position.y + 2, 1).onComplete +=
+            () => bubble.transform.DOMoveY(patate, 1).onComplete += movebubble;
+    }
     public GameObject GetMGPrefab() => mGPrefab;
 
     public GameObject GetDialogBox() => dialogBox;
