dacfa129e4a822566ec498d221ebe48555b4224a
Tom MASTANTUONO <compte.spidlle@gmail.com>
2024-03-22 17:35:09 +0100

    pseudotuto

--- Assets/Taptoplay.cs
+++ Assets/Taptoplay.cs
@@ -1,18 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Taptoplay : MonoBehaviour
 {
     [SerializeField] InputActionReference input;
-    
+    [SerializeField] ZombieProgression zombie;
+    [SerializeField] RunnerManager runner;
+
     // Start is called before the first frame update
     void Start()
     {
+        Character.Current.DisableInputs();
+        
         input.action.Enable();
-        input.action.started += context => Destroy(gameObject);
+        input.action.started += context =>
+        {
+            Character.Current.EnableInputs();
+            RoadsManager.StartMovement();
+            RoadsManager.SpeedUp();
+            zombie.demarreteszombie();
+            runner.startlejeu();
+            Destroy(gameObject);
+        };
     }
     
 
