0106a60134d99990e1d9fc4d20ef12909501c1cf
Skyron <courtier.leo@gmail.com>
2024-03-22 11:02:40 +0100

    Remove debug

--- Assets/Matteo/Script/UIManager.cs
+++ Assets/Matteo/Script/UIManager.cs
@@ -4,19 +4,9 @@ using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
-    public InputActionReference InputActionReference;
-    public InputAction InputAction => InputActionReference.action;
     
     [SerializeField] private TextMeshProUGUI beerTMP;
-
-    private void Start() {
-        InputAction.Enable();
-        InputAction.started += context => {
-            GameManager.SetBeerCount(1);
-            beerTMP.text = GameManager.GetBeerCount().ToString();
-        };
-    }
-
+    
     void Update() {
         if (Input.GetKeyDown(KeyCode.D)) GameManager.DeleteSaveFile();
     }
